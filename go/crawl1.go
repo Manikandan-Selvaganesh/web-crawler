fca3e852c0a3f36730a68927be393d491af05589
Manikandan Selvaganesh <mselvaga@redhat.com>
2016-06-21 11:31:58 +0530

    go : move go contents into a directory
    Signed-off-by: Manikandan Selvaganesh <mselvaga@redhat.com>

---
+++ go/crawl1.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+        "strings"
+	"golang.org/x/net/html"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: crawl http://manikandanselvaganesh.wordpress.com\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+func main() {
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+	fmt.Println(args)
+	if len(args) < 1 {
+		fmt.Println("Please specify start page\n\"crawl -h\" for Help")
+		os.Exit(1)
+	}
+
+	queue := make(chan string)
+	filteredQueue := make(chan string)
+
+	go func() { queue <- args[0] }()
+	go filterQueue(queue, filteredQueue)
+
+	// pull from the filtered queue, add to the unfiltered queue
+	for uri := range filteredQueue {
+		enqueue(uri, queue)
+	}
+}
+
+func filterQueue(in chan string, out chan string) {
+	var seen = make(map[string]bool)
+	for val := range in {
+		if !seen[val] {
+			seen[val] = true
+			out <- val
+		}
+	}
+}
+
+func enqueue(uri string, queue chan string) {
+	fmt.Println("fetching", uri)
+	transport := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+	}
+	client := http.Client{Transport: transport}
+	resp, err := client.Get(uri)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+
+	links := collectlink(resp.Body)
+
+	for _, link := range links {
+                // Parse every URL in the specified domain and the follow URL's
+			absolute := fixUrl(link, uri)
+			if uri != "" {
+				go func() { queue <- absolute }()
+			}
+	}
+}
+
+func fixUrl(href, base string) string {
+	uri, err := url.Parse(href)
+	if err != nil {
+		return ""
+	}
+	baseUrl, err := url.Parse(base)
+	if err != nil {
+		return ""
+	}
+	uri = baseUrl.ResolveReference(uri)
+	return uri.String()
+}
+
+// Package collectlinks does extraordinarily simple operation of parsing a given piece of html
+// and providing you with all the hyperlinks hrefs it finds.
+
+func collectlink(httpBody io.Reader) []string {
+	links := []string{}
+	col := []string{}
+	page := html.NewTokenizer(httpBody)
+	for {
+		tokenType := page.Next()
+		if tokenType == html.ErrorToken {
+			return links
+		}
+		token := page.Token()
+		if tokenType == html.StartTagToken && token.DataAtom.String() == "a" {
+			for _, attr := range token.Attr {
+				if attr.Key == "href" {
+					tl := trimHash(attr.Val)
+					col = append(col, tl)
+					resolv(&links, col)
+				}
+			}
+		}
+	}
+}
+
+// trimHash slices a hash # from the link
+func trimHash(l string) string {
+	if strings.Contains(l, "#") {
+		var index int
+		for n, str := range l {
+			if strconv.QuoteRune(str) == "'#'" {
+				index = n
+				break
+			}
+		}
+		return l[:index]
+	}
+	return l
+}
+
+// check looks to see if a url exits in the slice.
+func check(sl []string, s string) bool {
+	var check bool
+	for _, str := range sl {
+		if str == s {
+			check = true
+			break
+		}
+	}
+	return check
+}
+
+// resolv adds links to the link slice and insures that there is no repetition
+// in our collection.
+func resolv(sl *[]string, ml []string) {
+	for _, str := range ml {
+		if check(*sl, str) == false {
+			*sl = append(*sl, str)
+		}
+	}
+}
